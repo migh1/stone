edb92073db5770f74eaef69846a1aa7cada5c250
ojuara <lucas.araujo@ambar.tech>
2021-01-14 23:11:52 -0300

    refactor: atualiza config da instancia do database

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,11 @@
 import Loki from 'lokijs';
 
-const db = new Loki('src/database/db.json');
+const db = new Loki('src/database/db.json', {
+  autoload: true,
+  autoupdate: true,
+  autosave: true,
+  autosaveInterval: 5000,
+});
 
 db.loadDatabase();
 
