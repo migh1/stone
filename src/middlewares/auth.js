5111bfb5c07972199c1735997ced5f96171fb9fa
ojuara <lucas.araujo@ambar.tech>
2021-01-16 16:47:26 -0300

    feat: Remove validacao de expiração de tempo, lib faz isso automatico rs

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,13 +1,6 @@
 import httpStatus from 'http-status-codes';
 import { jwt } from '../utils';
 
-const isValidExpiration = (tokenUnix) => {
-  const today = new Date();
-  const todayUnix = parseInt(today.getTime() / 1000, 10);
-
-  return todayUnix < tokenUnix;
-};
-
 export default (req, res, next) => {
   const token = req.headers['authorization'];
 
@@ -15,10 +8,6 @@ export default (req, res, next) => {
 
   try {
     const decoded = jwt.validate(token);
-
-    if (!isValidExpiration(decoded.exp)) {
-      res.status(httpStatus.UNAUTHORIZED).send('Expired token');
-    }
 
     req.user = decoded;
     next();
--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -2,15 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const secret = '9=uh321ojp3i21j3bn6#$#*(HN4#@N$#';
 
-const expirationDate = () => {
-  const today = new Date();
-  const expirationDate = new Date(today);
-  expirationDate.setDate(today.getDate() + 60);
-
-  return parseInt(expirationDate.getTime() / 1000, 10);
-};
-
-const jwtSign = (email) => jwt.sign({ email }, secret, { expiresIn: expirationDate() });
+const jwtSign = (email) => jwt.sign({ email }, secret, { expiresIn: '10 min' });
 
 const jwtVerify = (token) => jwt.verify(token, secret);
 
